1d84011fdcc1e9d83a2f0b6c0b93ca78cb5ca66e
unknown <heetgoyani245@gmail.com>
2023-11-05 19:02:19 +0530

    2nd commit on system

--- 01_basics/helloworld.js
+++ 01_basics/helloworld.js
@@ -1,2 +1,5 @@
-console.log("Hello Heet")
-console.log("I'm Testing Again and Again")
+const key="I will not tell you";
+let name="No name";
+var id=420;
+let undef;
+console.table([key,name,id,undef]);
