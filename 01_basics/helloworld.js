02b54f88a817e681eb4655d54df01da0b1cb6c99
unknown <heetgoyani245@gmail.com>
2023-11-11 10:56:13 +0530

    03_TypeConversion.js

--- 01_basics/helloworld.js
+++ 01_basics/helloworld.js
@@ -3,3 +3,4 @@ let name="No name";
 var id=420;
 let undef;
 console.table([key,name,id,undef]);
+alert("It's Error 404");
---
+++ 03_TypeConversion.js
@@ -0,0 +1,4 @@
+let score="30abc";
+console.log(typeof score);
+let num = Number (score);
+console.log(typeof num);
