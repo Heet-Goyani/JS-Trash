5e3fafb8cee9aa3975213d138b175dc34b777701
Heet-Goyani <heetgoyani245@gmail.com>
2023-11-11 11:02:11 +0530

    03_typeConvirsion

--- 03_TypeConversion.js
+++ 03_TypeConversion.js
@@ -2,3 +2,8 @@ let score="30abc";
 console.log(typeof score);
 let num = Number (score);
 console.log(typeof num);
+console.log(num);
+let str= String(score);
+console.log(typeof str);
+console.log(str);
+
