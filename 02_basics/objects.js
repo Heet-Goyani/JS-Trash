e3fff98cd41da1d3a05e9121216f698b9bc038cd
Heet-Goyani <heetgoyani245@gmail.com>
2024-01-28 18:10:11 +0530

    Obj 2

--- 02_basics/objects.js
+++ 02_basics/objects.js
@@ -2,6 +2,6 @@ const myObj=new Object()
 myObj.Name="Heet",
 myObj.Roll_No="UI21EC30",
 myObj.Email="heet@mymail.com"
-console.log(myObj)
 const myob=myObj
 myob.Email="heetgoyani@mymail.com"
+console.log(myObj)
